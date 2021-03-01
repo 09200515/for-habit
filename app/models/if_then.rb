ce4660ea7a04b461655229c03a7c44d17f23757d
09200515 <a6t3lka23tsc8kg@i.softbank.jp>
2021-03-01 16:39:51 +0900

    目標管理モデルファイルの作成とマイグレート

---
+++ app/models/if_then.rb
@@ -0,0 +1,2 @@
+class IfThen < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "users#show"
 
+  resources :objectives, only: [:new, :create]
 end
